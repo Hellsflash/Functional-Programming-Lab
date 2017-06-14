24cbe2f83f300a515c0c15cb5fb0c31e69a5b3ea
Mario Slavov <hellsflash@gmail.com>
2017-06-14 03:25:07 +0300

    Add project files.

---
+++ Count Uppercase Words/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine()
+                .Split(new string[] {" "},StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+            text.Where(checker)
+                .ToList()
+                .ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
---
+++ Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(input.Count);
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
